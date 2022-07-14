f02df28ab6bec7b3aeb4f3e805ed5235c45183e4
Gorbataras <47342116+Gorbataras@users.noreply.github.com>
2022-07-14 13:07:14 -0700

    Implement login

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import {getAuth} from 'firebase/auth'
+import {useEffect, useState} from 'react'
 
 function Profile() {
+
+  const [User, setUser] = useState(null)
+  const auth = getAuth()
+  useEffect(() => {
+    setUser(auth.currentUser)
+  }, [])
+  
+  return User ? <h1>{User.displayName}</h1> : 'Not Logged In'
+
   return (
     <div>Profile</div>
   )
