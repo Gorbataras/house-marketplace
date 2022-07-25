a5e6cfb5309c7e39babfda0f2e6484124db1ac82
Gorbataras <47342116+Gorbataras@users.noreply.github.com>
2022-07-25 11:47:51 -0700

    add ability to change name on profile page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import Navbar from './components/Navbar'
 import Explore from './pages/Explore'
 import ForgotPassword from './pages/ForgotPassword';
@@ -22,8 +24,9 @@ function App() {
       </Routes>
 
     <Navbar/>
-
     </Router>
+
+    <ToastContainer />
 
     </>
   );
